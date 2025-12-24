cc911981b96de42dfdf5425a0f5475ec8073d2e0
ronikriger <krigerroni@gmail.com>
2025-12-24 09:35:57 -0300

    Add nanoid dependency and update TypeScript version

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  generateBuildId: async () => {
-    return 'build-' + Date.now();
-  },
+    images: {
+        unoptimized: true,
+    },
+    generateBuildId: async () => {
+        return 'build-' + Date.now();
+    },
 };
 
 module.exports = nextConfig;
