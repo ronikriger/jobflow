dd0e3cefb5bd99c6ce629103cf6a3d65996405dd
ronikriger <krigerroni@gmail.com>
2026-01-16 21:52:30 -0300

    Fix StackHandler with explicit app prop for auth redirect

--- src/app/handler/[...stack]/page.tsx
+++ src/app/handler/[...stack]/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { StackHandler } from "@stackframe/stack";
+import { stackClientApp } from "@/stack/client";
 
 export default function Handler() {
-    return <StackHandler fullPage />;
+    return <StackHandler fullPage app={stackClientApp} />;
 }
